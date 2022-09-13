7a0b437047d7fd608e27bd58cd40ec6802219698
Charles <achigbulamcharles49@web.de>
2022-09-13 05:01:04 -0700

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,31 +1,24 @@
 #!/usr/bin/python3
+"""docstring for Square"""
+
+
 class Square:
-    """Represents a square.
-    Private instance attribute: size:
-        - property def size(self)
-        - property setter def size(self, value)
-    Instantiation with optional size.
-    Public instance method: def area(self).
-    """
-
+    """docstring for init"""
     def __init__(self, size=0):
-        """Initializes the data."""
-        self.__size = size
-
+        self.size = size
+    """size getter method"""
     @property
     def size(self):
-        """Retrieves the size."""
         return self.__size
-
+    """size setter method"""
     @size.setter
     def size(self, value):
-        """Sets the size to a value."""
-        if not isinstance(value, int):
+        if isinstance(value, int) and value >= 0:
+            self.__size = value
+        elif not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value
-
+    """calculate the area of square"""
     def area(self):
-        """Returns the current square area."""
-        return self.__size ** 2
+        return (self.size ** 2)
